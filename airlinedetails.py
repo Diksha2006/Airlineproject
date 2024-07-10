0ac59fa1c3c13491b3357f9c878070c57bf5f78e
sakshimalwadkar <sakshimalwadkar2005@gmail.com>
2024-07-10 11:05:10 +0530

    details.py

---
+++ airlinedetails.py
@@ -0,0 +1,5 @@
+import csv
+a_id=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+a_name=["Indigo Airlines","Kohinoor Airlines","Qatar Airlines","AirAsia India","Air India Airlines","Air India Express Airlines","Go First Airlines ","SpiceJet Airlines","Vistara Airlines","Jet Airways","Singapore Airlines","Vistara Airlines","Emirates Airlines","Akash Air","Delta Airlines","Air France","Turkish Airlines","WestJet Airlines","JetBlue Airlines","AirAsia airlines"]
+a_Type=["Domestic","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic","International","Domestic"]
+a_location=[]
