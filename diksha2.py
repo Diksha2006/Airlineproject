2b134e896f122bdcf34cdb36a9226dd527391a39
sakshimalwadkar <sakshimalwadkar2005@gmail.com>
2024-07-10 20:08:26 +0530

    details

--- diksha2.py
+++ diksha2.py
@@ -1,2 +1,7 @@
-print("HII.....")
-print("I AM DIKSHA")
+import csv
+def read_csv():
+    with open("airlinedetails.csv",mode="r",newline="") as file:
+        
+        red = csv.DictReader(file)
+        return list(red)
+print(read_csv())
